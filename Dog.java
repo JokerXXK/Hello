069d027abd31dc6fed2d712f4dd1115767c2eede
JokerXXK <xiangxinkong18@gmail.com>
2025-12-09 02:42:48 +0800

    create

---
+++ Dog.java
@@ -0,0 +1,38 @@
+public class Dog {
+
+    public int WeightInPounds;
+    public static String binomen = "Canis familiaris";
+
+    public Dog(int w){
+        WeightInPounds = w;
+    }
+
+    public void makeNoise(){
+        if (WeightInPounds < 10) {
+            System.out.println("Yip!");
+        }else if (WeightInPounds < 30) {
+            System.out.println("bark!");
+            
+        }else{
+            System.out.println("woooof!");
+        }
+        
+    }
+
+    public static Dog maxDog(Dog d1, Dog d2){
+        if(d1.WeightInPounds > d2.WeightInPounds){
+            return d1;
+        }else{
+            return d2;
+        }
+    }
+
+    public Dog maxDog(Dog d2){
+        if(this.WeightInPounds > d2.WeightInPounds){
+            return this;
+        }else{
+            return d2;
+        }
+    }
+
+}
